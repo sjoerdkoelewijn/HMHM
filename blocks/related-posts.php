4454e64866ff94a6e8f1724ccbf1b80fafe34a69
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-07-07 17:16:30 +0200

    fixed < 3 related posts style

--- blocks/related-posts.php
+++ blocks/related-posts.php
@@ -28,29 +28,46 @@ $id = 'related-posts' . $block['id'];
         
         if( $posts ): 
             
-            if( count($posts) < 4 ) {
+            if( count($posts) < 4 ) { ?>
+
+                <div class="related_posts_inner">
                 
-                global $post;
+                    <?php global $post;
 
-                foreach( $posts as $post ): ?>
+                    foreach( $posts as $post ): ?>
 
-                <?php setup_postdata($post); ?>
+                    <?php setup_postdata($post); ?>
 
                     <div class="post">
-                        <a class="title" href="<?php the_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                        <p class="excerpt">
-                            <?php the_excerpt(); ?>
-                        </p>
-                        <a class="read_more" href="<?php the_permalink(); ?>">
-                            <?php _e( 'Read More', 'hashmuseum' ) ?>
-                        </a>
+
+                        <a class="image_wrap" href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail( 'large' ); ?>
+                        </a> 
+
+                        <div class="text_wrap">
+
+                            <a class="title" href="<?php the_permalink(); ?>">
+                                <h2>
+                                    <?php the_title(); ?>
+                                </h2>
+                            </a>
+                            <p class="excerpt">
+                                <?php the_excerpt(); ?>
+                            </p>
+                            <a class="read_more" href="<?php the_permalink(); ?>">
+                                <?php echo file_get_contents(get_template_directory_uri() . "/images/svg/arrowRightIcon.svg"); ?>
+                                <?php _e( 'Read More', 'hashmuseum' ) ?>
+                            </a>
+
+                        </div>
+
                     </div>                     
 
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
 
-                <?php wp_reset_postdata(); ?>
+                    <?php wp_reset_postdata(); ?>
+
+                </div>
 
             <?php } else { ?>
 
