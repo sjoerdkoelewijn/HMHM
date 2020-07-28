df1f971628d72ea7bbd7619445e7340c4ee9f6bc
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-07-28 14:38:56 +0200

    added custom image sizes

--- functions.php
+++ functions.php
@@ -326,7 +326,8 @@ add_action( 'created_info_categories', 'sk_created_info_category', 10, 3 );
 
 /*************************** Register Custom Thumbnail sizes *********************************/
 
-	add_image_size( 'larger_image', 3840, 2560, false ); 
+	add_image_size( 'largest_image', 3840, 2560, false );
+	add_image_size( 'larger_image', 1920, 1280, false );  
 	add_image_size( 'large_image', 1620, 1080, false ); 
 	add_image_size( 'medium_image', 960, 638, false ); 
     add_image_size( 'small_image', 300, 300, true ); // (cropped)
