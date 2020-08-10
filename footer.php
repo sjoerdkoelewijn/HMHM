61ef81043bb2953d7a03a4d51e36a7d0fbfd083e
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-08-10 16:25:36 +0200

    added favicon

--- footer.php
+++ footer.php
@@ -207,15 +207,12 @@
 
 	</body>
 
-	<?php if(strpos($_SERVER['HTTP_HOST'], '.local') !== true){ ?>
+	<?php if(strpos($_SERVER['HTTP_HOST'], 'hashmuseum.local') !== false){ ?>
 		<script>
 		document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
 		</script>
-
-
-	<?php 
-		echo 'local';
-} ?>
+		
+	<?php } ?>
 
 	
 
--- functions.php
+++ functions.php
@@ -6,10 +6,28 @@ include 'functions/gutenberg-blocks.php';
 include 'functions/enqueue.php';
 include 'functions/menus.php';
 
-/* ACF php file is not imported on local env */
-if(strpos($_SERVER['HTTP_HOST'], '.local') !== true){
+if(strpos($_SERVER['HTTP_HOST'], 'hashmuseum.local') !== false){
+	
+	function favicon() {
+		echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_template_directory_uri().'/images/localfavicon.ico" />';
+	}
+	add_action('wp_head', 'favicon');
+
+} else {
+
+	/* ACF php file is not imported on local env */
 	include 'functions/advanced-custom-fields.php';	
-}
+
+	function favicon() {
+		echo '<link rel="Shortcut Icon" type="image/x-icon" href="'. get_template_directory_uri() . '/images/favicon.ico" />';
+	}
+	add_action('wp_head', 'favicon');
+
+};
+
+
+
+
 
 add_theme_support( 'title-tag' );
 add_theme_support( 'menus' );
