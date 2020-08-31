b3504983e5dd1a39972b800a16537f4f0e3b05bf
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-08-31 14:50:17 +0200

    mobile style info block 2 image

--- blocks/info-two-images.php
+++ blocks/info-two-images.php
@@ -6,78 +6,94 @@ $id = 'info-two-image' . $block['id'];
 
 ?>
 
-    <article id="<?php echo $id; ?>" class="info_two_image">
+    <article id="<?php echo $id; ?>" class="info_two_image">      
         
+
+        <?php 
+        $imageleft = get_field('image_left');
+        $imageright = get_field('image_right');
+        $mobileimage = get_field('mobile_image');
+        $quotetoggle = get_field('quote_toggle');
+        $quote = get_field('quote');
+        $quoteauthor = get_field('quote_author');
+                    
+        if ($quotetoggle === 'left_quote'): ?>                
+
+            <div class="quote_wrap">
+
+                <?php include('parts/global/quote.php'); ?>
+
+            </div> 
+
+        <?php else : 
+
+            if( $imageleft ): ?>
+
+            <div class="image_wrap">
+
+                <img loading="lazy" class="image" src="<?php echo esc_url($imageleft['sizes']['large']); ?>" alt="<?php echo $image['alt']; ?>" />
+
+                <?php if($imageleft['caption']) { ?>
+
+                    <p class="caption">
+                        <?php echo esc_html($imageleft['caption']); ?></?php>
+                    </p>
+
+                <?php } ?>  
+            
+            </div>    
+
+            <?php endif;
+
+        endif; ?>
+
+        <div class="image_wrap mobile">
+
+            <img loading="lazy" class="image" src="<?php echo esc_url($mobileimage['sizes']['large']); ?>" alt="<?php echo $mobileimage['alt']; ?>" />
+
+            <?php if($mobileimage['caption']) { ?>
+
+                <p class="caption">
+                    <?php echo esc_html($mobileimage['caption']); ?></?php>
+                </p>
+
+            <?php } ?>  
         
-
-            <?php 
-            $imageleft = get_field('image_left');
-            $imageright = get_field('image_right');
-            $quotetoggle = get_field('quote_toggle');
-            $quote = get_field('quote');
-            $quoteauthor = get_field('quote_author');
-                        
-            if ($quotetoggle === 'left_quote'): ?>                
-
-                <div class="quote_wrap">
-
-                    <?php include('parts/global/quote.php'); ?>
-
-                </div> 
-    
-            <?php else : 
-
-                if( $imageleft ): ?>
-
-                <div class="image_wrap">
-
-                    <img loading="lazy" class="image" src="<?php echo esc_url($imageleft['sizes']['large']); ?>" alt="<?php echo $image['alt']; ?>" />
-
-                    <?php if($imageleft['caption']) { ?>
-
-                        <p class="caption">
-                            <?php echo esc_html($imageleft['caption']); ?></?php>
-                        </p>
-
-                    <?php } ?>  
-                
-                </div>    
-
-                <?php endif;
-
-            endif; ?>
+        </div> 
+        
 
         <?php include('parts/info/info-text.php'); ?>
 
-            <?php if ($quotetoggle === 'right_quote'): ?>
 
-                <div class="quote_wrap">
+        <?php if ($quotetoggle === 'right_quote'): ?>
 
-                    <?php include('parts/global/quote.php'); ?>
+            <div class="quote_wrap">
 
-                </div>
-    
-            <?php else : 
+                <?php include('parts/global/quote.php'); ?>
 
-                if( $imageright ): ?>
+            </div>
 
-                <div class="image_wrap">
+        <?php else : 
 
-                    <img loading="lazy" class="image" src="<?php echo esc_url($imageright['sizes']['large']); ?>" alt="<?php echo $image['alt']; ?>" />
+            if( $imageright ): ?>
 
-                    <?php if($imageright['caption']) { ?>
+            <div class="image_wrap">
 
-                        <p class="caption">
-                            <?php echo esc_html($imageright['caption']); ?></?php>
-                        </p>
+                <img loading="lazy" class="image" src="<?php echo esc_url($imageright['sizes']['large']); ?>" alt="<?php echo $image['alt']; ?>" />
 
-                    <?php } ?>  
-                
-                </div> 
+                <?php if($imageright['caption']) { ?>
 
-                <?php endif;
+                    <p class="caption">
+                        <?php echo esc_html($imageright['caption']); ?></?php>
+                    </p>
 
-            endif; ?>
+                <?php } ?>  
+            
+            </div> 
+
+            <?php endif;
+
+        endif; ?>
         
 
     </article>
