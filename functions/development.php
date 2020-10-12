3b841e3851aafd007bd3ff2f132183c6239e826a
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-10-12 17:30:41 +0200

    added gdpr cookies v1

--- functions/development.php
+++ functions/development.php
@@ -12,7 +12,7 @@ if(strpos($_SERVER['HTTP_HOST'], 'hashmuseum.local') !== false){
 } else {
 
 	/* ACF php file is not imported on local env */
-	include 'functions/advanced-custom-fields.php';	
+	include 'advanced-custom-fields.php';	
 
 	function favicon() {
 		echo '<link rel="Shortcut Icon" type="image/x-icon" href="'. get_template_directory() . '/images/favicon.ico" />';
