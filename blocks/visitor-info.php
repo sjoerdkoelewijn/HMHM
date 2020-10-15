38f3eeedf947be7d28023487b584cb0b7d57e611
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-10-15 11:40:11 +0200

    added caption to visitor info image

--- blocks/visitor-info.php
+++ blocks/visitor-info.php
@@ -14,6 +14,13 @@ $id = 'visitor-information-' . $block['id'];
 
         <?php if( !empty( $image ) ): ?>
             <img loading="lazy" class="image" src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo $image['alt']; ?>" />
+        
+            <?php if($image['caption']) { ?>
+                <p class="caption">
+                    <?php echo esc_html($image['caption']); ?></?php>
+                </p>
+            <?php } ?>
+            
         <?php endif; ?>
 
     </div>
