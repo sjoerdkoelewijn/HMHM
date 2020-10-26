e1a9c3b0783d0ad2ade09cb557643ea47dd7b76b
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-10-26 15:25:08 +0100

    added collection overview translations

--- archive-collection.php
+++ archive-collection.php
@@ -2,7 +2,38 @@
 
 <?php
 	
-	$collection_custom_page   = get_page_by_path('collection', OBJECT, 'collection_pages');
+	$current_language = pll_current_language();
+
+	switch ($current_language) {
+	case 'en':	
+		$new_url = str_replace('/collectionpage/', '/collection/', $current_url);
+		redirect($new_url, 302);
+		break;
+	case 'ca':
+		$collection_custom_page   = get_page_by_path('colleccio', OBJECT, 'collection_pages');
+		
+		break;
+	case 'es':
+		$collection_custom_page   = get_page_by_path('collection', OBJECT, 'collection_pages');
+		
+		break;
+	case 'fr':
+		$collection_custom_page   = get_page_by_path('collection', OBJECT, 'collection_pages');
+		
+		break;
+	case 'nl':
+		$collection_custom_page   = get_page_by_path('collectie', OBJECT, 'collection_pages');
+		
+		break;			
+	case 'de':
+		$collection_custom_page   = get_page_by_path('sammlung', OBJECT, 'collection_pages');
+		
+		break;			
+	default:
+		$collection_custom_page   = get_page_by_path('collection', OBJECT, 'collection_pages');
+		
+	}
+	
 	$collection_output =  apply_filters( 'the_content', $collection_custom_page->post_content );
 
 	if ( empty($collection_output) ) :
