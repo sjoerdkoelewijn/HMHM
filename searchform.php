62abb3f90c67e0d434dbfd08ee1c4abd49311e0b
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-11-12 17:10:33 +0100

    added label to search bar

--- searchform.php
+++ searchform.php
@@ -1,6 +1,8 @@
 <form role="search" method="get" class="search_form" action="<?php echo esc_url(home_url('/')); ?>">    
+<label for="search">Search function</label>
+
     <input type="search" name="s" id="search" class="search_field" placeholder="<?php _e( 'Type to search &hellip;', 'hashmuseum' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>">
-    <button type="submit" class="search_submit">
+    <button aria-label="search" type="submit" class="search_submit">
         <img loading="lazy" class="icon" src="<?php echo get_theme_file_uri() ?>/images/svg/searchIcon.svg" alt="Search Icon">
     </button>
 </form>
